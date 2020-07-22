41df07bf2b13f7a8b2fff27ace109cdf5f85463a
JoBa <jbayron1122@gmail.com>
2020-07-22 18:24:22 -0500

    Resultado final

--- app.js
+++ app.js
@@ -4,116 +4,71 @@
   angular.module('ShoppingListCheckOff',[]) // create angular module: food
   .controller('ToBuyController', ToBuyController) // create controller that's called ToBuyController
   .controller('AlreadyBoughtController', AlreadyBoughtController) // create controller that's called AlreadyBoughtController
-  .provider('ToBuyControllerService', ToBuyControllerServiceProvider)
-  .provider('AlreadyBoughtControllerService',AlreadyBoughtControllerServiceProvider) //AlreadyBoughtControllerServiceProvider)
-  .config(Config);
-  
-  Config.$inject = ['ToBuyControllerServiceProvider'];
-function Config(ToBuyControllerServiceProvider) {
-  //no used
-}
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+
 //==================== buy controller ==============================================
-ToBuyController.$inject = ['$scope','ToBuyControllerService','AlreadyBoughtControllerService'];
-function ToBuyController($scope,ToBuyControllerService, AlreadyBoughtControllerService) {
-  var buyList = this;
-  buyList.items = ToBuyControllerService.getItems();
-  try{
-    if(1 == 3) throw "empty";
-    $scope.valor = buyList.items[4];
-
-  }
-  catch (err)
-  {      $scope.valor = "entro";
-
-    buyList.errorMessage = "" + err;
-  }
-  buyList.moveToBought = function (item,itemIndex) {
-    var item = ToBuyControllerService.removeItem(item,itemIndex);
-    AlreadyBoughtControllerService.addItemToBuy(item.name, item.quantity);
-  };
+ToBuyController.$inject = ['$scope','ShoppingListCheckOffService'];
+function ToBuyController($scope,ShoppingListCheckOffService) {
+  var toBuyList = this;
+  toBuyList.items = ShoppingListCheckOffService.getItemsToBuy();
+  
+  toBuyList.moveToBought = function (item,itemIndex) {
+    var itemDelete = ShoppingListCheckOffService.removeItem(item,itemIndex);
+    ShoppingListCheckOffService.addItem(itemDelete.name, itemDelete.quantity);
+  }  
+};
+//==================== AlreadyBoughtController controller ==============================================
+AlreadyBoughtController.$inject = ['$scope','ShoppingListCheckOffService'];
+function AlreadyBoughtController($scope,ShoppingListCheckOffService) {
+  var bougthList = this;
+     bougthList.items = ShoppingListCheckOffService.getItemsBought();
 }
-//==================== bought controller ==============================================
-AlreadyBoughtController.$inject = ['AlreadyBoughtControllerService'];
-function AlreadyBoughtController(AlreadyBoughtControllerService) {
-  var boughtList = this;
-  boughtList.items = AlreadyBoughtControllerService.getItems();
- }
-//==================================================================================
-//================= service to buycontroller =======================================
-function ToBuyControllerService(defaultList) {
+//================= service =======================================
+function ShoppingListCheckOffService() {
   var service = this;
   // List of shopping items
-  var toBuyItems = defaultList;
- 
+  var toBuy = [
+    {
+      name: "Milk",
+      quantity: "2"
+    },
+    {
+      name: "Donuts",
+      quantity: "200"
+    },
+    {
+      name: "Cookies",
+      quantity: "300"
+    },
+    {
+      name: "bread",
+      quantity: "30"
+    },
+    {
+      name: "Chocolate",
+      quantity: "5"
+    }
+  ];
+  var bougth =  [];
+
+  service.addItem = function (itemname,itemquantity){
+    var item = {
+      name: itemname,
+      quantity: itemquantity
+    }
+    bougth.push(item);
+  };
   service.removeItem = function (item,itemIndex) {
-    toBuyItems.splice(itemIndex, 1);
+    toBuy.splice(itemIndex, 1);
     return item;
   };
-  service.getItems = function () {
-    return toBuyItems;
+  service.getItemsToBuy = function () {
+      return toBuy;
   };
-}
-//=============== PROVIDER DE EL CONTROLADOR ToBuyControllerProvider ===============
-  function ToBuyControllerServiceProvider() {
-    var provider = this;
-    provider.defaults = {
-      defaultList : [
-        {
-          name: "Milk",
-          quantity: "2"
-        },
-        {
-          name: "Donuts",
-          quantity: "200"
-        },
-        {
-          name: "Cookies",
-          quantity: "300"
-        },
-        {
-          name: "bread",
-          quantity: "30"
-        },
-        {
-          name: "Chocolate",
-          quantity: "5"
-        }
-      ]
-    };
-    provider.$get = function () {
-      var ToBuy = new ToBuyControllerService(provider.defaults.defaultList);
-  
-      return ToBuy;
-    };
-  }
-//================= service to buycontroller =======================================
-function AlreadyBoughtControllerService() {
-  var service = this;
-  var boughtItems = [];
-
-  service.addItemToBuy = function (itemName, quantity) {
-    var item = {
-     name: itemName,
-     quantity: quantity
-   };
-   boughtItems.push(item);
-  }
-   service.getItems = function () {
-      return boughtItems;
+  service.getItemsBought = function () {
+      return bougth;
   };
+ 
 }
-// // PROVIDER DE EL CONTROLADOR AlreadyBoughtControllerProvider
-
-  function AlreadyBoughtControllerServiceProvider() {
-    var provider = this;
-    provider.defaults = {
-    //no default values
-    };
-  
-    provider.$get = function () {
-       var shoppingList = new AlreadyBoughtControllerService();
-  
-       return shoppingList;
-    };
-  }
 })();
